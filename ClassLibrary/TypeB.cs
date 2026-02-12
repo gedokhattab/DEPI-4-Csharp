9358be9cdc494e4c88601c1aa173afe83ecbf0b9
Khattab Abd El-Moniem Khattab Mohamed <141855324+gedokhattab@users.noreply.github.com>
2026-02-12 08:42:47 +0200

    Add files via upload

---
+++ ClassLibrary/TypeB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class TypeB : TypeA
+    {
+        public void Print()
+        {
+            //Console.WriteLine(F); // F is private in TypeA, so it can't be accessed in TypeB.
+            Console.WriteLine(G); // G is internal in TypeA, so it can be accessed in TypeB.
+            Console.WriteLine(H); // H is public in TypeA, so it can be accessed in TypeB.
+        }
+    }
+}

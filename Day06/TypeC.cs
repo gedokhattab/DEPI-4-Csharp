a24a9e3f167e95c7a348993351e1dcbe1e68e70d
Khattab Abd El-Moniem Khattab Mohamed <141855324+gedokhattab@users.noreply.github.com>
2026-02-12 08:44:21 +0200

    Add files via upload

---
+++ Day06/TypeC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Day06
+{
+    internal class TypeC : TypeA
+    {
+        public void Print()
+        {
+            //Console.WriteLine(F); // F is private in TypeA, so it can't be accessed in TypeC.
+            //Console.WriteLine(G); // G is internal in TypeA, so it can be accessed in TypeC.
+            Console.WriteLine(H); // H is public in TypeA, so it can be accessed in TypeC.
+        }
+    }
+}

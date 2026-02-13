1a480296ef7fd178732c532e5f6146779ce55de2
Khattab Abd El-Moniem Khattab Mohamed <141855324+gedokhattab@users.noreply.github.com>
2026-02-13 15:17:11 +0200

    Add files via upload

---
+++ Day07/Interfaces/IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    internal interface IShape
+    {
+        public int Area { get; }
+
+        public void Draw();
+
+        public void PrintDetails();
+    }
+}
